f79ba10899f8145cd06e7352d783b9e5e42268b5
L3odr0id <leon.leonc@yandex.ru>
2022-02-09 17:42:39 +0300

    update docs

--- bin/asset_fill.dart
+++ bin/asset_fill.dart
@@ -1,5 +1,6 @@
 import 'package:asset_fill/asset_fill.dart' as asset_fill;
 
+/// executable function
 void main() {
   asset_fill.main();
 }
--- lib/src/writer.dart
+++ lib/src/writer.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:yaml_edit/yaml_edit.dart';
 
+/// Writes tohe result to the pubspec.yaml
 class Writer {
   static void writeDataToYAML(List<String> paths) {
     final pubspec = File('pubspec.yaml');
